b4c7d07f06ac480e600c90b835cbcc7128611c58
Gregory <gregory@MacBook-Pro-de-Gregory.local>
2026-04-18 14:12:18 -0400

    Proyecto Hola Mundo React con Docker

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <img src={reactLogo} className="logo" alt="React logo" />
+      <h1>Hola Mundo</h1>
+      <p>Practica de Entrega Continua - DevOps</p>
+      <p className="author">Gregory</p>
+    </div>
+  )
+}
+
+export default App
